0e62d61ff7a51c6639f03de5b2d89b9cb317ff4b
Alchemist997 <brainbrain160@gmail.com>
2021-11-01 18:19:30 +0300

    Добавил анимации 2-й странице

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,24 +1,15 @@
 $('#fullPage').fullpage({
     sectionSelector: '.part',
+    scrollOverflow: true,
     afterLoad: function (link, index) {
         if (index == 2) {
             // alert(index);
             const anim = gsap.timeline();
-
             anim.to('.s2__content', { duration: 0.7, x: '-100%' })
-                // .to()
-
-            // let main = document.querySelector('#fullPage');
-
-            // ScrollTrigger.create({
-            //     animation: anim,
-            //     trigger: '#fullPage',
-            //     start: 'top',
-            //     // end: 'bottom',
-            //     end: () => main.offsetWidth / 2,
-            //     scrub: true,
-            //     pin: true
-            // });
+                .fromTo('#img1', { height: 0 }, { height: 'auto', duration: 0.5 })
+                .fromTo('#img3', { height: 0 }, { height: 'auto', duration: 0.7 })
+                .fromTo('#img2', { width: 0 }, { width: '42%', duration: 0.5 })
+                .fromTo('.s2-top__left span', { opacity: 0 }, { opacity: 1, duration: 1.5 });
         }
     }
 });
