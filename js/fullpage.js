cf4f1254503aaf19004dbf1a03dee2d458f0a5e2
Alchemist997 <brainbrain160@gmail.com>
2021-11-01 18:48:33 +0300

    Мелкая правка для хедера

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,8 +1,11 @@
+let header = document.querySelector('.main-header');
+
 $('#fullPage').fullpage({
     sectionSelector: '.part',
     scrollOverflow: true,
     afterLoad: function (link, index) {
         if (index == 2) {
+            header.classList.add('white')
             // alert(index);
             const anim = gsap.timeline();
             anim.fromTo('.s2__content', {x: 0}, { duration: 0.7, x: '-100%' })
@@ -10,6 +13,8 @@ $('#fullPage').fullpage({
                 .fromTo('#img3', { height: 0 }, { height: 'auto', duration: 0.7 })
                 .fromTo('#img2', { width: 0 }, { width: '42%', duration: 0.5 })
                 .fromTo('.s2-top__left span', { opacity: 0 }, { opacity: 1, duration: 1.5 });
+        } else {
+            header.classList.remove('white')
         }
     }
 });
