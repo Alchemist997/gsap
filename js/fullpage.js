0db31a924a972d4c7e91149eff36432f648b07fc
Alchemist997 <brainbrain160@gmail.com>
2021-11-01 17:09:43 +0300

    Добавил переход на 2 часть

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,3 +1,24 @@
 $('#fullPage').fullpage({
-    sectionSelector: '.part'
+    sectionSelector: '.part',
+    afterLoad: function (link, index) {
+        if (index == 2) {
+            // alert(index);
+            const anim = gsap.timeline();
+
+            anim.to('.s2__content', { duration: 0.7, x: '-100%' })
+                // .to()
+
+            // let main = document.querySelector('#fullPage');
+
+            // ScrollTrigger.create({
+            //     animation: anim,
+            //     trigger: '#fullPage',
+            //     start: 'top',
+            //     // end: 'bottom',
+            //     end: () => main.offsetWidth / 2,
+            //     scrub: true,
+            //     pin: true
+            // });
+        }
+    }
 });
