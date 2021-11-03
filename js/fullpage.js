52a9d1dd1b72592ca859b6cd77e506db3b3ad2b2
Alchemist997 <brainbrain160@gmail.com>
2021-11-03 13:08:43 +0300

    Return from s2 to intro only if s2 view on top

--- js/fullpage.js
+++ js/fullpage.js
@@ -2,6 +2,19 @@ let header = document.querySelector('.main-header');
 let bmwBtn = document.querySelector('#bmwBtn');
 let leftSlide = document.querySelector('.s1');
 let rightSlide = document.querySelector('.s2');
+let rightSlideWrap;
+
+let rightSlideWrapFinder = setInterval(() => {
+    let targetEl = rightSlide.querySelector('.fp-scroller');
+    if (targetEl) {
+        rightSlideWrap = targetEl;
+        clearInterval(rightSlideWrapFinder);
+    }
+}, 50);
+
+setTimeout(() => {
+    clearInterval(rightSlideWrapFinder);
+}, 5000);
 
 $('#fullPage').fullpage({
     sectionSelector: '.part',
@@ -27,21 +40,30 @@ tl.to('#img1 .img-cap', { top: '100%', duration: 0.7, delay: 0.7 })
     .fromTo('.s2-top__left', { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.pause();
 
+let rightOnTop = 0;
+
 document.addEventListener('wheel', evt => {
-    console.log(rightSlide.offsetTop);
     if (evt.deltaY > 0 && leftSlide.classList.contains('sCurrent')) {
         $.fn.fullpage.moveSlideRight();
         leftSlide.classList.remove('sCurrent');
         rightSlide.classList.add('sCurrent');
         tl.play();
-    } else if (evt.deltaY < 0 && !leftSlide.classList.contains('sCurrent')) {
-        setTimeout(() => {
-            $.fn.fullpage.moveSlideLeft();
-        }, 1700);
-        leftSlide.classList.add('sCurrent');
-        rightSlide.classList.remove('sCurrent');
-        tl.reverse();
+    } else if (evt.deltaY < 0 && !leftSlide.classList.contains('sCurrent') &&
+        rightSlideWrap.style.transform.indexOf('translate(0px)') > -1) {
+
+        if (rightOnTop <= 3) {
+            rightOnTop++;
+        } else {
+            rightOnTop = 0;
+            setTimeout(() => {
+                $.fn.fullpage.moveSlideLeft();
+            }, 1700);
+            leftSlide.classList.add('sCurrent');
+            rightSlide.classList.remove('sCurrent');
+            tl.reverse();
+        }
     }
+    if (evt.deltaY > 0) rightOnTop = 0;
 });
 
 bmwBtn.addEventListener('click', () => {
