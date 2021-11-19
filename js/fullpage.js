8a3e87a74a44526ea6edb30a5d45cb5f2a1e5e9e
Alchemist997 <brainbrain160@gmail.com>
2021-11-19 21:54:42 +0300

    Изменил горизонтальный переход

--- js/fullpage.js
+++ js/fullpage.js
@@ -20,7 +20,6 @@ $('#fullPage').fullpage({
     sectionSelector: '.part',
     // autoScrolling: false,
     scrollOverflow: true,
-    // scrollBar: true,
     controlArrows: false,
     onSlideLeave: function (section, origin, destination, direction) {
         if (destination == 0) {
@@ -34,7 +33,8 @@ $('#fullPage').fullpage({
 });
 
 let tl = gsap.timeline();
-tl.to('#img1 .img-cap', { top: '100%', duration: 0.7, delay: 0.7 })
+tl.fromTo('.s1__slide-cover', { x: '100%' }, { x: 0, duration: 1 })
+    .to('#img1 .img-cap', { top: '100%', duration: 0.7, delay: 0 })
     .to('#img2 .img-cap', { bottom: '100%', duration: 0.7, delay: -0.7 })
     .to('#img3 .img-cap', { left: '100%', duration: 0.7, delay: -0.7 })
     .fromTo('.s2-top__left', { opacity: 0 }, { opacity: 1, duration: 1 });
@@ -44,18 +44,21 @@ let rightOnTop = 0;
 
 document.addEventListener('wheel', evt => {
     if (evt.deltaY > 0 && leftSlide.classList.contains('sCurrent')) {
-        $.fn.fullpage.moveSlideRight();
+        setTimeout(() => {
+            $.fn.fullpage.silentMoveTo(0, 1);
+        }, 900);
         leftSlide.classList.remove('sCurrent');
         rightSlide.classList.add('sCurrent');
         tl.play();
     } else if (evt.deltaY < 0 && !leftSlide.classList.contains('sCurrent') && rightSlideWrap) {
-        if (rightSlideWrap.style.transform.indexOf('translate(0px)') > -1 || rightSlideWrap.style.transform.indexOf('translate(0px, 0px)') > -1) {
+        if (rightSlideWrap.style.transform.indexOf('translate(0px)') > -1 ||
+            rightSlideWrap.style.transform.indexOf('translate(0px, 0px)') > -1) {
             if (rightOnTop <= 3) {
                 rightOnTop++;
             } else {
                 rightOnTop = 0;
                 setTimeout(() => {
-                    $.fn.fullpage.moveSlideLeft();
+                    $.fn.fullpage.silentMoveTo(0, 0);
                 }, 1700);
                 leftSlide.classList.add('sCurrent');
                 rightSlide.classList.remove('sCurrent');
@@ -68,7 +71,7 @@ document.addEventListener('wheel', evt => {
         } else {
             rightOnTop = 0;
             setTimeout(() => {
-                $.fn.fullpage.moveSlideLeft();
+                $.fn.fullpage.silentMoveTo(0, 0);
             }, 1700);
             leftSlide.classList.add('sCurrent');
             rightSlide.classList.remove('sCurrent');
@@ -79,7 +82,9 @@ document.addEventListener('wheel', evt => {
 });
 
 bmwBtn.addEventListener('click', () => {
-    $.fn.fullpage.moveSlideRight();
+    setTimeout(() => {
+        $.fn.fullpage.silentMoveTo(0, 1);
+    }, 900);
     leftSlide.classList.remove('sCurrent');
     rightSlide.classList.add('sCurrent');
     tl.play();
